2eb19092a875d701facbd5a5f2401ffc4789c192
Kelvin Lau <kclau95@gmail.com>
2016-09-16 17:35:12 +1200

    Added Long id

--- src/main/java/domain/Item.java
+++ src/main/java/domain/Item.java
@@ -11,12 +11,20 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Item {
 	
+	private Long id;
+	
 	private String name;
 	private Long stockLevel;
 	private BigDecimal price;
 	private Set<Image> images;
 	// TODO Category?
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getName() {
 		return name;
 	}
