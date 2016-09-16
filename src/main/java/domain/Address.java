0406b4b5724d2ade00f1df0b009109f31ddc134b
Kelvin Lau <kclau95@gmail.com>
2016-09-16 16:44:04 +1200

    Added fields for Address and Image

--- src/main/java/domain/Address.java
+++ src/main/java/domain/Address.java
@@ -1,5 +1,40 @@
 package domain;
 
 public class Address {
-
+	private int number;
+	private String street;
+	private String suburb;
+	private String city;
+	private String zipCode;
+	
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getSuburb() {
+		return suburb;
+	}
+	public void setSuburb(String suburb) {
+		this.suburb = suburb;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
 }
--- src/main/java/domain/Image.java
+++ src/main/java/domain/Image.java
@@ -3,5 +3,26 @@ package domain;
 public class Image {
 	
 	private String name;
+	private int width;
+	private int height;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
 
 }
