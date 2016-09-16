c8289d368d3f6e1e71d895500913bf2c7ef8fcdc
Kelvin Lau <kclau95@gmail.com>
2016-09-16 19:18:01 +1200

    Constructor for Image

--- src/main/java/domain/Image.java
+++ src/main/java/domain/Image.java
@@ -8,6 +8,15 @@ public class Image {
 	private String name;
 	private int width;
 	private int height;
+	
+	protected Image(){
+	}
+	
+	public Image(String name, int width, int height){
+		this.name = name;
+		this.width = width;
+		this.height = height;
+	}
 
 	public String getName() {
 		return name;
