8824770b920a95ad674d17cb6fc8f1a3dea78c01
Kelvin Lau <kclau95@gmail.com>
2016-09-16 17:29:36 +1200

    Added simple xml @s

--- src/main/java/domain/Item.java
+++ src/main/java/domain/Item.java
@@ -3,6 +3,12 @@ package domain;
 import java.math.BigDecimal;
 import java.util.Set;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Item {
 	
 	private String name;
