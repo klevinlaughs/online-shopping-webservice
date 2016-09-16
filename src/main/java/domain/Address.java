05d1d5c3b20624077be00d67b322bb65fcbc8d4f
Kelvin Lau <kclau95@gmail.com>
2016-09-16 17:42:41 +1200

    Added country

--- src/main/java/domain/Address.java
+++ src/main/java/domain/Address.java
@@ -5,6 +5,7 @@ public class Address {
 	private String street;
 	private String suburb;
 	private String city;
+	private String country;
 	private String zipCode;
 	
 	public int getNumber() {
@@ -30,6 +31,12 @@ public class Address {
 	}
 	public void setCity(String city) {
 		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
 	}
 	public String getZipCode() {
 		return zipCode;
