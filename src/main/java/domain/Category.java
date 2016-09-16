ea591123203b9c7e1ae76a7ab1770a6d3ede680d
Kelvin Lau <kclau95@gmail.com>
2016-09-16 17:40:02 +1200

    Added xml @s for Category, uses id and idref

--- src/main/java/domain/Category.java
+++ src/main/java/domain/Category.java
@@ -1,10 +1,27 @@
 package domain;
 
+import javax.xml.bind.annotation.XmlID;
+import javax.xml.bind.annotation.XmlIDREF;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Category {
 	
+	private Long id;
+	@XmlID
+	private String xmlId;
+	
 	private String name;
+	@XmlIDREF
 	private Category parentCategory;
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+		xmlId = getClass().getName() + ":" + id;
+	}
 	public String getName() {
 		return name;
 	}
