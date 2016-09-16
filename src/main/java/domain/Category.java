c6848bd4fe0c7a92409ea4478cdb7073be178dec
Kelvin Lau <kclau95@gmail.com>
2016-09-16 17:04:05 +1200

    Added parentCategory to Category

--- src/main/java/domain/Category.java
+++ src/main/java/domain/Category.java
@@ -3,5 +3,19 @@ package domain;
 public class Category {
 	
 	private String name;
+	private Category parentCategory;
 	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Category getParentCategory() {
+		return parentCategory;
+	}
+	public void setParentCategory(Category parentCategory) {
+		this.parentCategory = parentCategory;
+	}
+
 }
