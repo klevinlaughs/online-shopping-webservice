ed1ee81690c0e76825cad421ec472ed5fd636194
Kelvin Lau <kclau95@gmail.com>
2016-09-17 13:19:05 +1200

    Added isDealItem=false
    also autoformat

--- src/main/java/domain/Item.java
+++ src/main/java/domain/Item.java
@@ -13,53 +13,80 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Item {
-	
+
 	private Long id;
-	
+
 	private String name;
 	private Long stockLevel;
 	private BigDecimal price;
-	@XmlElementWrapper(name="Images")
-	@XmlElement(name="Image")
+	@XmlElementWrapper(name = "Images")
+	@XmlElement(name = "Image")
 	private Set<Image> images = new HashSet<Image>();
-	// TODO Category?
-	
+	private Category category;
+	private boolean dealItem = false;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Long getStockLevel() {
 		return stockLevel;
 	}
+
 	public void setStockLevel(Long stockLevel) {
 		this.stockLevel = stockLevel;
 	}
+
 	public BigDecimal getPrice() {
 		return price;
 	}
+
 	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
+
 	public Set<Image> getImages() {
 		return images;
 	}
+
 	public void setImages(Set<Image> images) {
 		this.images = images;
 	}
-	
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public boolean isDealItem() {
+		return dealItem;
+	}
+
+	public void setDealItem(boolean dealItem) {
+		this.dealItem = dealItem;
+	}
+
 	/**
 	 * Gets the rating for an item
+	 * 
 	 * @return
 	 */
-	public double getRating(){
+	public double getRating() {
 		// TODO
 		return 0.0;
 	}
