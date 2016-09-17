ea382294127a3d328b6f4c23939732884792a9bc
Kelvin Lau <kclau95@gmail.com>
2016-09-18 02:31:19 +1200

    Category - made xmlid transient, defined ManyToOne relation for parent

--- src/main/java/domain/Category.java
+++ src/main/java/domain/Category.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlID;
@@ -20,11 +22,13 @@ public class Category {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 	@XmlID
+	@Transient
 	private String xmlId;
 
 	@Column(nullable = false, unique = true)
 	private String name;
 	@XmlIDREF
+	@ManyToOne
 	private Category parentCategory;
 
 	/**
