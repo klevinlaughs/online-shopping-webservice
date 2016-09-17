5bc33a141dcf661824c601b13c487d8fe9431a34
Kelvin Lau <kclau95@gmail.com>
2016-09-17 17:01:08 +1200

    Added non-nullable columns and constructors for Image

--- src/main/java/domain/Image.java
+++ src/main/java/domain/Image.java
@@ -1,5 +1,6 @@
 package domain;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -7,9 +8,42 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Embeddable
 public class Image {
 
+	@Column(nullable = false)
 	private String name;
 	private int width;
 	private int height;
+
+	/**
+	 * Default constructor needed for persistent classes
+	 */
+	protected Image() {
+	}
+
+	/**
+	 * Takes required name for image
+	 * 
+	 * @param name
+	 *            the name of the image
+	 */
+	public Image(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Takes required name of image and optional width and height dimensions
+	 * 
+	 * @param name
+	 *            the name of the image
+	 * @param width
+	 *            the width of the image
+	 * @param height
+	 *            the height of the image
+	 */
+	public Image(String name, int width, int height) {
+		this.name = name;
+		this.width = width;
+		this.height = height;
+	}
 
 	public String getName() {
 		return name;
