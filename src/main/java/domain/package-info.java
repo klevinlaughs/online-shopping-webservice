e37f67c12f7a888ab15b10adc5aff2ca23fcab75
Kelvin Lau <kclau95@gmail.com>
2016-09-18 00:09:06 +1200

    Added DateTimeAdapter and package-info

---
+++ src/main/java/domain/package-info.java
@@ -0,0 +1,13 @@
+@XmlJavaTypeAdapters({
+    @XmlJavaTypeAdapter(type=DateTime.class,
+        value=DateTimeAdapter.class)
+})
+
+package domain;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
+ 
+import jaxb.DateTimeAdapter;
+
+import org.joda.time.DateTime;
