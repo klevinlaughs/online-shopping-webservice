3e96c54aa37ba19df4488a42c05dd9461c801531
Kelvin Lau <kclau95@gmail.com>
2016-09-17 17:20:52 +1200

    Added non-null col and constructor to Category

--- src/main/java/domain/Category.java
+++ src/main/java/domain/Category.java
@@ -1,5 +1,6 @@
 package domain;
 
+import javax.persistence.Column;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlID;
@@ -14,9 +15,39 @@ public class Category {
 	@XmlID
 	private String xmlId;
 
+	@Column(nullable = false)
 	private String name;
 	@XmlIDREF
 	private Category parentCategory;
+
+	/**
+	 * Default constructor needed for persistent classes
+	 */
+	protected Category() {
+	}
+
+	/**
+	 * Creates a category with no parent. Parent can be set
+	 * 
+	 * @param name
+	 *            the name of the category
+	 */
+	public Category(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Creates a sub-category under the specified parent category
+	 * 
+	 * @param name
+	 *            the name of the category
+	 * @param parentCategory
+	 *            the parent category
+	 */
+	public Category(String name, Category parentCategory) {
+		this.name = name;
+		this.parentCategory = parentCategory;
+	}
 
 	public Long getId() {
 		return id;
