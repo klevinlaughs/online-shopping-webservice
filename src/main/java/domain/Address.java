c97e148525eba98d7c437af4e3fefd0cea8e92b7
Kelvin Lau <kclau95@gmail.com>
2016-09-17 13:47:42 +1200

    Address is now embeddable

--- src/main/java/domain/Address.java
+++ src/main/java/domain/Address.java
@@ -1,11 +1,13 @@
 package domain;
 
+import javax.persistence.Embeddable;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
+@Embeddable
 public class Address {
 	private int number;
 	private String street;
