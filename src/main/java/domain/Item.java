4c6823858edbc5c82055be3501b738b96ef3778c
Kelvin Lau <kclau95@gmail.com>
2016-09-18 02:09:51 +1200

    Added SingleImageAdapter so only one Image is marshalled ofr an item

--- src/main/java/domain/Item.java
+++ src/main/java/domain/Item.java
@@ -15,6 +15,9 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import jaxb.SingleImageAdapter;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -30,6 +33,7 @@ public class Item {
 	private Long stockLevel;
 	@Column(nullable = false)
 	private BigDecimal price;
+	@XmlJavaTypeAdapter(SingleImageAdapter.class)
 	@XmlElementWrapper(name = "Images")
 	@XmlElement(name = "Image")
 	@ElementCollection
