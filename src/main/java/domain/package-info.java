3fed628c984ae8e569a77f4931a99987077b1272
Kelvin Lau <kclau95@gmail.com>
2016-09-19 19:23:55 +1200

    Cleaned up package-info.java

--- src/main/java/domain/package-info.java
+++ src/main/java/domain/package-info.java
@@ -1,19 +1,13 @@
 @XmlJavaTypeAdapters({
     @XmlJavaTypeAdapter(type=DateTime.class,
         value=DateTimeAdapter.class)
-//    ,
-//    @XmlJavaTypeAdapter(type=List.class,
-//    	value=PurchaseHistoryAdapter.class)
 })
 
 package domain;
 
-import java.util.List;
-
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
- 
-import jaxb.DateTimeAdapter;
-import jaxb.PurchaseHistoryAdapter;
 
 import org.joda.time.DateTime;
+
+import jaxb.DateTimeAdapter;
