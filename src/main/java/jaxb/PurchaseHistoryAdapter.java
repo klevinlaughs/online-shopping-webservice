c9aee2d65fa861b7a33539621249c2dd178229ea
Kelvin Lau <kclau95@gmail.com>
2016-09-19 19:13:50 +1200

    Commented on PurchaseHistoryAdapter

--- src/main/java/jaxb/PurchaseHistoryAdapter.java
+++ src/main/java/jaxb/PurchaseHistoryAdapter.java
@@ -7,20 +7,28 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 import domain.Item;
 
-public class PurchaseHistoryAdapter extends XmlAdapter<List<Item>, List<Item>> {
+/**
+ * An XmlAdapter to return a limited amount of items from a user's
+ * purchaseHistory. The marshalled type can't be an interface
+ * 
+ * @author Kelvin
+ *
+ */
+public class PurchaseHistoryAdapter extends XmlAdapter<ArrayList<Item>, List<Item>> {
 
 	@Override
-	public List<Item> unmarshal(List<Item> v) throws Exception {
+	public List<Item> unmarshal(ArrayList<Item> v) throws Exception {
 		return v;
 	}
 
 	@Override
-	public List<Item> marshal(List<Item> v) throws Exception {
-		List<Item> items = new ArrayList<Item>(5);
+	public ArrayList<Item> marshal(List<Item> v) throws Exception {
+		ArrayList<Item> items = new ArrayList<Item>(5);
 		for (int i = 0; i < 5; i++) {
 			try {
 				items.add(v.get(i));
 			} catch (IndexOutOfBoundsException e) {
+				// If less than 5, then return all that
 				break;
 			}
 		}
