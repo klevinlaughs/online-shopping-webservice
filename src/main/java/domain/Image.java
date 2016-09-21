bc66a2162683e2982dbadd638b3fd405f13c13ff
Kelvin Lau <kclau95@gmail.com>
2016-09-21 13:52:36 +1200

    Image hashCode and equals override

--- src/main/java/domain/Image.java
+++ src/main/java/domain/Image.java
@@ -4,6 +4,9 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 @XmlRootElement
 @Embeddable
 public class Image {
@@ -75,6 +78,24 @@ public class Image {
 		sb.append("Image:{ name:" + name).append(", width:" + width)
 				.append(", height" + height).append(" }");
 		return sb.toString();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Image))
+			return false;
+		if (obj == this)
+			return true;
+
+		Image rhs = (Image) obj;
+		return new EqualsBuilder().append(name, rhs.name)
+				.append(width, rhs.width).append(height, rhs.height).isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17, 31).append(name).append(width)
+				.append(height).toHashCode();
 	}
 
 }
